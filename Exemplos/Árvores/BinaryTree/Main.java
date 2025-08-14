93f8a75aeedf46a3a9c494a25a529084ac094ad7
Antonio Tadeu Maffeis <amaffeis@fatecitu.local>
2025-08-14 16:35:44 -0300

    Aula-14-08-2025-16-35-00

--- Exemplos/Árvores/BinaryTree/Main.java
+++ Exemplos/Árvores/BinaryTree/Main.java
@@ -6,19 +6,13 @@ public class Main {
         Scanner in = new Scanner(System.in);
         System.out.println("Binary Tree");
         BinaryTree btree = new BinaryTree();
-        int vet[] = new int[7];
         
         for (int i=0; i < 7; i++)
         {
             System.out.print("-> ");
-            vet[i] = in.nextInt();
+            btree.add(btree.getRoot(), new Node(in.nextInt()));
         }
-        
-        for (int i=0; i < vet.length; i++)
-        {
-            btree.add(btree.getRoot(), new Node(vet[i]));
-        }
-        
+                
         btree.show();
     }
 }
